f798d22057498a1bc169b0b5200225a39fd68a74
Rithik <rithik@example.com>
2025-01-16 19:23:44 +0530

    first

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.get('/', (req, res) => {
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://MERN-bookstore:P6UBNKFmmBI1AAc7@mern-bookstore.ogw04.mongodb.net/?retryWrites=true&w=majority&appName=MERN-bookstore";
 
+const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {

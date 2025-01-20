f43f566041d6e98667c770342611db6dbf50c27d
Rithik <rithik@example.com>
2025-01-20 18:25:19 +0530

    changed the path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const cors  = require("cors");
-
+require('dotenv').config();
 
 // password 
 // P6UBNKFmmBI1AAc7
@@ -10,6 +10,12 @@ const cors  = require("cors");
 
 app.use(cors());
 app.use(express.json());
+
+const allowedOrigins = ["https://rkbookstore.vercel.app/"];
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PATCH", "DELETE"]
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -20,7 +26,8 @@ app.get('/', (req, res) => {
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://MERN-bookstore:P6UBNKFmmBI1AAc7@mern-bookstore.ogw04.mongodb.net/?retryWrites=true&w=majority&appName=MERN-bookstore";
+const uri = process.env.MONGODB_URI;
+"mongodb+srv://MERN-bookstore:P6UBNKFmmBI1AAc7@mern-bookstore.ogw04.mongodb.net/?retryWrites=true&w=majority&appName=MERN-bookstore";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -108,7 +115,7 @@ async function run() {
    
     // Send a ping to confirm a successful connection
      await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     
@@ -118,5 +125,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  //console.log(`Example app listening on port ${port}`)
 })
